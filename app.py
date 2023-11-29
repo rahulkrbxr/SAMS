e8bf45601832b75d5780a93f3c2d10f232e25846
rahulkrbxr <61363513+rahulkrbxr@users.noreply.github.com>
2023-11-29 20:39:21 +0000

    login for student and admin

--- app.py
+++ app.py
@@ -3,8 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def login():
-  return render_template('login.html')
+def home():
+  return render_template("home.html")
+
+@app.route("/student/")
+def student_login():
+  return render_template('student_login.html')
+
+
+@app.route("/admin/")
+def admin_login():
+  return render_template('admin_login.html')
 
 
 if __name__ == "__main__":
